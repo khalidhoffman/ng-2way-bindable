362b276606f544ce1909a3110a8871fcdc9ba17e
Khalid Hoffman <khalidhoffman@gmail.com>
2017-11-29 02:12:55 -0500

    post cleanup

--- index.ts
+++ index.ts
@@ -3,48 +3,44 @@ import set = Reflect.set;
 
 export const BindableInput = function () {
   return function (target: any, paramKey: string) {
-    const paramSafeKey = `_${paramKey}_bindable`;
     const paramEmitterKey = `${paramKey}Change`;
-    const store: { [key: string]: any } = {};
-    const ngSet = function (val) {
-      store[paramSafeKey] = val;
-      store[paramEmitterKey].emit(store[paramSafeKey]);
-    };
-
-    const ngGet = function () {
-      return store[paramSafeKey];
-    };
-
-    console.warn('initializing emitter');
-    store[paramEmitterKey] = new EventEmitter<string>();
-
+	const store: { [key: string]: any } = {[paramEmitterKey]: new EventEmitter<string>()};
     const targetPropertyMeta = {
       enumerable: true,
       configurable: true
     };
+	const targetPropertyEmitterMeta = {
+      enumerable: true,
+      configurable: true,
+      writable: true,
+      value: store[paramEmitterKey]
+    };
+    const ngSet = function (val) {
+      store[paramKey] = val;
+      store[paramEmitterKey].emit(store[paramKey]);
+    };
+
+    const ngGet = function () {
+      return store[paramKey];
+    };
 
     const targetPropertyAccessors = {
       set: function (val: any) {
-        if (!store.done) {
-          console.warn('binding set accessor');
+        if (!store[paramEmitterKey].instance) {
           this[paramEmitterKey] = store[paramEmitterKey];
           this[set [paramKey]] = ngSet;
-          store.done = true;
+          store[paramEmitterKey].instance = this;
         }
-        console.log(`setting/emitting ${val}, %o`, this);
         ngSet(val);
       },
       get: ngGet
     };
 
-    Object.defineProperty(target, paramKey, Object.assign(targetPropertyMeta, targetPropertyAccessors));
-    Object.defineProperty(target, paramEmitterKey, {
-      enumerable: true,
-      configurable: true,
-      writable: true,
-      value: store[paramEmitterKey]
-    });
-    Input().apply(this, arguments);
+	Object.assign(targetPropertyMeta, targetPropertyAccessors)
+    Object.defineProperty(target, paramKey, targetPropertyMeta);
+    Object.defineProperty(target, paramEmitterKey, targetPropertyEmitterMeta);
+
+    Input().apply(this, [target, paramKey]);
     Output().apply(this, [target, paramEmitterKey]);
   }
 };
